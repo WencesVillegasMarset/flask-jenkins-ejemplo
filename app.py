098125b4422d894b286c5466125212cf77698cbc
WencesVillegasMarset <villegaswences@gmail.com>
2019-06-26 17:53:24 -0300

    eliminada basura y modificada script

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 from flask import Flask
 app = Flask(__name__)
-
+bd = {
+    1:'Juan',
+    2:'Mikael',
+    3:'Roberto',
+    4:'Jose',
+    5:'Carlos',
+}
 @app.route('/')
 @app.route('/hello')  # this route is not working
 @app.route('/hello/')
@@ -11,7 +17,7 @@ def hello_world():
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
     # show the user profile for that user
-    return 'Why Hello %s!\n' % username
+    return 'Hola  %s!\n' % username
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')  # open for everyone
